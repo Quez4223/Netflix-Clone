6ff54a34d5454ef0b2cf409e4d07f91e46389ea1
MarQuez LeDay <quezleday@MarQuezs-MBP.attlocal.net>
2021-05-17 13:36:20 -0400

    Setup for Netflix Clone

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hey Quez! Let's build Netflix Clone Front-end today</h1>
+
+    </div>
+    
+    
+  );
+}
+
+export default App;
