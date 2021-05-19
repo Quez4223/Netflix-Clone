d4df143f846e495e61549809b6494cd2979f1ab2
MarQuez LeDay <quezleday@MarQuezs-MBP.attlocal.net>
2021-05-19 08:26:26 -0400

    Added axios and request.js

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div className='App'>
       <h1>Hey Quez! Let's build Netflix Clone Front-end today</h1>
-
-    </div>
-    
-    
+    </div>  
   );
 }
 
---
+++ netflix-clone/src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = '43455add6950b71d1125dfa9c5b38156'
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key${API_KEY}&with_generes=28`,
+    fetchComedyMovies: `/discover/movie?api_key${API_KEY}&with_generes=35`,
+    fetchHorrorMovies: `/discover/movie?api_key${API_KEY}&with_generes=27`,
+    fetchRomanaceMovies: `/discover/movie?api_key${API_KEY}&with_generes=10749`,
+    fetchDocumentaries: `/discover/movie?api_key${API_KEY}&with_generes=99`,
+}
+
+export default requests; 
