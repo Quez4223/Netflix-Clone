59776015396547fca92d123aa91d66974eb3dfb9
MarQuez LeDay <quezleday@MarQuezs-MBP.attlocal.net>
2021-05-20 15:43:58 -0400

    Fixed rendering of rest of movies and started Nav bar

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Row from './Row'
 import requests from './requests'
 import Banner from './Banner'
+import Nav from './Nav'
 
 function App() {
   return (
@@ -15,11 +16,11 @@ function App() {
       
       <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
       <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
-      {/* <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
       <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
       <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
       <Row title='Romance Movies' fetchUrl={requests.fetchRomanaceMovies} />
-      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} /> */}
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
     </div>  
   );
 }
---
+++ netflix-clone/src/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div className='nav'>
+            <img 
+            className='nav_logo' 
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015-logo.svc/1920px-Netflix_2015_logo.svc.png'
+            alt='Netflix Logo'
+            />
+            <img 
+            className='nav avatar' 
+            src='https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png'
+            alt='Avatar'
+            />
+        </div>
+    )
+}
+
+export default Nav
